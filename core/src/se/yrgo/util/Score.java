a39d9c5acdeb1b661ef4e634f3a0c3337b5dd68f
jessica abony <jessica_abony@hotmail.com>
2023-04-13 21:46:22 +0200

    methods to handle all time high score

--- core/src/se/yrgo/util/Score.java
+++ core/src/se/yrgo/util/Score.java
@@ -1,11 +1,17 @@
 package se.yrgo.util;
 
+import java.io.*;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Score {
     private static Integer score;
     private static Integer highScore;
+    //private static File top10File;
+    private TreeMap<Integer,String> allTimeHighScores = new TreeMap<>(Collections.<Integer>reverseOrder());
 
     public static void setScore(float birdPosition) {
-
         score = (int)birdPosition / 10;
     }
 
@@ -21,4 +27,42 @@ public class Score {
     public static String getHighScore() {
         return highScore.toString();
     }
+
+
+
+  /*  3 metoder:
+    - registrera nytt highscore på alltimehighscore listan
+    - skriva in nya highscoret i filen
+    - printa ut all time high scores*/
+
+    public void registerAllTimeHighScore(String playername, Integer highScore) {
+        Integer top1Score = allTimeHighScores.firstKey();
+        if (highScore > top1Score) {
+            allTimeHighScores.put(highScore, playername);
+            saveAllTimeHighScoresToFile();
+        }
+    }
+
+    public void saveAllTimeHighScoresToFile() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("top10.txt"))) {
+            for (Map.Entry<Integer, String> entry : allTimeHighScores.entrySet()) {
+                bw.write(entry.getKey() + "," + entry.getValue());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public void printAllTimeHighScore() {
+        try (BufferedReader br = new BufferedReader(new FileReader("top10.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
 }
