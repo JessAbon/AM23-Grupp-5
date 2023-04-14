6b76aca3b6cee94c287733fe6306c7e03d18964d
Tilda Kampman <tilda.kampman@gmail.com>
2023-04-14 17:38:26 +0200

    fix buttons in endScreen

--- core/src/se/yrgo/screens/EndScreen.java
+++ core/src/se/yrgo/screens/EndScreen.java
@@ -9,12 +9,10 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Sort;
 import com.badlogic.gdx.utils.TimeUtils;
 import se.yrgo.JumpyBirb;
 import se.yrgo.Sprites.Button;
 import se.yrgo.util.Score;
-import se.yrgo.util.Util;
 
 public class EndScreen implements Screen {
 
@@ -26,21 +24,23 @@ public class EndScreen implements Screen {
 
     private Texture gameOver;
 
-    private Texture playBtn;
+    private Texture playTexture;
 
-    private Texture playBtnPressed;
+    private Texture playTexturePressed;
 
-    private Texture homeBtn;
+    private Texture homeTexture;
 
-    private Texture homeBtnPressed;
+    private Texture homeTexturePressed;
 
-    private Texture stopBtn;
+    private Texture stopTexture;
 
-    private Texture stopBtnPressed;
-    private Rectangle play;
-    private Rectangle quit;
-    private Rectangle mainMenu;
-    private Button playTestbtn;
+    private Texture stopTexturePressed;
+
+    private Button playButton;
+
+    private Button homeButton;
+
+    private Button stopButton;
 
     public EndScreen(final JumpyBirb game) {
         this.game = game;
@@ -49,46 +49,25 @@ public class EndScreen implements Screen {
         timeStamp = TimeUtils.millis();
         gLayout = new GlyphLayout();
 
-        //playTestbtn = new Button(Util.getGlobalPositionZeroX(), 0 , 100 ,100);
+
 
         gameOver = new Texture("menu/Bg.png");
-        playBtn = new Texture("menu/PlayTest.png");
-        playBtnPressed = new Texture("menu/Play-pressed.png");
-        homeBtn = new Texture("menu/Home.png");
-        homeBtnPressed = new Texture("menu/Home-pressed.png");
-        stopBtn = new Texture("menu/Stop.png");
-        stopBtnPressed = new Texture("menu/Stop-pressed.png");
+        playTexture = new Texture("menu/PlayTest.png");
+        playTexturePressed = new Texture("menu/Play-pressed.png");
+        homeTexture = new Texture("menu/Home.png");
+        homeTexturePressed = new Texture("menu/Home-pressed.png");
+        stopTexture = new Texture("menu/Stop.png");
+        stopTexturePressed = new Texture("menu/Stop-pressed.png");
 
-        //play = new Rectangle(200, 450, 100, 100);
-        quit = new Rectangle(370, 450, 100, 100);
-        mainMenu = new Rectangle(500, 450, 100, 100);
-        //play = new Rectangle(190, 60, playBtn.getWidth(), playBtn.getHeight());
 
-        playTestbtn = new Button(0, 0 , playBtn.getWidth() ,playBtn.getHeight());
+        playButton = new Button(190, 50, playTexture.getWidth(), playTexture.getHeight());
+        homeButton = new Button(365, 50, homeTexture.getWidth(), homeTexture.getHeight());
+        stopButton = new Button(550, 50, stopTexture.getWidth(), stopTexture.getHeight());
 
         /*ShapeRenderer shapeRenderer = new ShapeRenderer();
-        shapeRenderer.setProjectionMatrix(camera.combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-        Vector3 v = camera.unproject(new Vector3(playTestbtn.getBoundsButton().x, playTestbtn.getBoundsButton().y, 0),
-                0, 0, camera.viewportWidth, camera.viewportHeight);
-
-        shapeRenderer.rect(v.x, v.y, playTestbtn.getBoundsButton().width, playTestbtn.getBoundsButton().height);
-        shapeRenderer.end();
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.rect(playTestbtn.getBoundsButton().x, playTestbtn.getBoundsButton().y, playTestbtn.getBoundsButton().width, playTestbtn.getBoundsButton().height);
-        shapeRenderer.end();
-
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.rect(quit.x, quit.y, quit.width, quit.height);
+        shapeRenderer.rect(playButton.getBoundsButton().x, playButton.getBoundsButton().y, playButton.getBoundsButton().width, playButton.getBoundsButton().height);
         shapeRenderer.end();*/
-
-
-        //shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        //shapeRenderer.rect(mainMenu.x, mainMenu.y, mainMenu.width, mainMenu.height);
-        //shapeRenderer.end();
     }
 
     @Override
@@ -99,56 +78,47 @@ public class EndScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.T)) {
-            System.out.println("bounds: x= "+ playTestbtn.getBoundsButton().x + "y= " + playTestbtn.getBoundsButton().y);
-
-            System.out.println(playTestbtn.getPositionButton().x);
-            System.out.println(playTestbtn.getPositionButton().y);
-
-        }
-
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
         game.batch.draw(gameOver, 0, 0, JumpyBirb.WIDTH, JumpyBirb.HEIGHT);
-        game.batch.draw(homeBtn, 0, 0, JumpyBirb.WIDTH, JumpyBirb.HEIGHT);
-        game.batch.draw(stopBtn, 0, 0, JumpyBirb.WIDTH, JumpyBirb.HEIGHT);
-        gLayout.setText(game.font, "SCORE: " + Score.getScore());
 
+        game.batch.draw(playTexture, playButton.getPositionButton().x, playButton.getPositionButton().y);
+        game.batch.draw(homeTexture, homeButton.getPositionButton().x, homeButton.getPositionButton().y);
+        game.batch.draw(stopTexture, stopButton.getPositionButton().x, stopButton.getPositionButton().y);
+
+        gLayout.setText(game.font, "SCORE: " + Score.getScore());
         game.font.draw(game.batch, gLayout, JumpyBirb.WIDTH / 2F - gLayout.width / 2F, JumpyBirb.HEIGHT / 2.7F + gLayout.height * 2);
         gLayout.setText(game.font, "HIGHSCORE: " + Score.getHighScore());
         game.font.draw(game.batch, gLayout, JumpyBirb.WIDTH / 2F - gLayout.width / 2F, JumpyBirb.HEIGHT / 3F + gLayout.height);
-        game.batch.draw(playBtn, 190, 60, playBtn.getWidth(), playBtn.getHeight());
 
 
-        game.batch.draw(playBtn, playTestbtn.getPositionButton().x, playTestbtn.getPositionButton().y);
+
 
 
 
         if (Gdx.input.isTouched()) {
-            int x = Gdx.input.getX();
-            int y = Gdx.input.getY();
 
-            System.out.println("x= "+ x + " y= " + y );
+            Vector3 click = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(click);
 
-
-            if (playTestbtn.getBoundsButton().contains(x, y - JumpyBirb.HEIGHT + playBtn.getHeight())) {
+            if (playButton.getBoundsButton().contains(click.x, click.y)) {
                 System.out.println("PLAY END");
                 game.setScreen(new GameScreen(game));
                 dispose();
-            } else if (quit.contains(x, y)) {
+            } else if (stopButton.getBoundsButton().contains(click.x, click.y)) {
                 System.out.println("QUIT");
                 //Gdx.app.exit();
                 dispose();
-            } else if (mainMenu.contains(x, y)) {
+            } else if (homeButton.getBoundsButton().contains(click.x, click.y)) {
                 System.out.println("MAIN MENU");
                 game.setScreen(new MainMenuScreen(game));
                 dispose();
             }
         }
-            game.batch.end();
+        game.batch.end();
 
-            //restart();
+        //restart();
 
     }
 
