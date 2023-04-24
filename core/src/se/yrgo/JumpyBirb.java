7627b354c61b1b5ae47e6e44a20bc69940160ffb
Tilda Kampman <tilda.kampman@gmail.com>
2023-04-24 20:32:19 +0200

    clean before push

--- core/src/se/yrgo/JumpyBirb.java
+++ core/src/se/yrgo/JumpyBirb.java
@@ -9,37 +9,38 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import se.yrgo.screens.MainMenuScreen;
 
 public class JumpyBirb extends Game {
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 600;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
 
-	private Music music;
-	public SpriteBatch batch;
-	public BitmapFont font;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		font = new BitmapFont(Gdx.files.internal("Fonts/Font3.1.fnt"));
-		this.setScreen(new MainMenuScreen(this));
-		music = Gdx.audio.newMusic(Gdx.files.internal("sound/radish-music.mp3"));
-		music.setLooping(true);
-		music.setVolume(0.1f);
-		music.play();
-	}
+    private Music music;
+    public SpriteBatch batch;
+    public BitmapFont font;
 
-	@Override
-	public void render () {
-		super.render();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		font.dispose();
-		music.dispose();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont(Gdx.files.internal("Fonts/Font3.1.fnt"));
+        this.setScreen(new MainMenuScreen(this));
+        music = Gdx.audio.newMusic(Gdx.files.internal("sound/radish-music.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.1f);
+        music.play();
+    }
 
-	}
-	public boolean spaceAndMouseClickInput() {
-		return Gdx.input.isKeyJustPressed(Input.Keys.SPACE) || Gdx.input.justTouched();
-	}
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+        music.dispose();
+
+    }
+
+    public boolean spaceAndMouseClickInput() {
+        return Gdx.input.isKeyJustPressed(Input.Keys.SPACE) || Gdx.input.justTouched();
+    }
 }
