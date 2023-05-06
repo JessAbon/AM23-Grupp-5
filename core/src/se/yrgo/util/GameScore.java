3e13ce5bf999856f5f366b717a63f47f89e4cbfc
jessica abony <jessica_abony@hotmail.com>
2023-05-06 17:37:52 +0200

    shows current highscore according to current difficulty level

--- core/src/se/yrgo/util/GameScore.java
+++ core/src/se/yrgo/util/GameScore.java
@@ -2,18 +2,29 @@ package se.yrgo.util;
 
 public class GameScore {
     private static Integer score;
-    private static Integer highScore;
+    private static Integer easyHighScore = 0;
+    private static Integer mediumHighScore = 0;
+    private static Integer hardHighScore = 0;
 
     public static void setScore(float birdPosition) {
         score = (int) birdPosition / 10;
     }
 
     public static void setBestScore() {
-        if (highScore == null) {
-            highScore = 0;
+        if (Settings.getLevel() == 2) {
+            if (score > hardHighScore) {
+                hardHighScore = score;
+            }
         }
-        if (score > highScore) {
-            highScore = score;
+        else if (Settings.getLevel() == 1) {
+            if (score > mediumHighScore) {
+                mediumHighScore = score;
+            }
+        }
+        else {
+            if (score > easyHighScore) {
+                easyHighScore = score;
+            }
         }
     }
 
@@ -22,12 +33,19 @@ public class GameScore {
     }
 
     public static String getHighScoreString() {
-        return highScore.toString();
+        if (Settings.getLevel() == 2) {
+            return hardHighScore.toString();
+        }
+        else if (Settings.getLevel() == 1) {
+            return mediumHighScore.toString();
+        }
+        else {
+            return easyHighScore.toString();
+        }
     }
 
     public static Integer getScore() {
         return score;
     }
-
 
 }
